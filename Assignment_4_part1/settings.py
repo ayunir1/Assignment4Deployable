41485275a52b78c7a4bbdafcd9ac9d27ae42f666
Blake Kinnaman <ayunir1@yahoo.com>
2023-04-18 13:43:48 -0500

    Static Updated

--- Assignment_4_part1/settings.py
+++ Assignment_4_part1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)r#&vxpwx@33ygh1!ywv#8(squul7%%w3$loo4%!fa-6@6e9ky'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -124,7 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
